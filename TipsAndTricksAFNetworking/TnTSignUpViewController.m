144732cd3e1c9313ab3e6c46efa50e301dbeba5d
Vivek Pandya <vivekvpandya@gmail.com>
2014-07-11 22:36:06 +0530

    code clean up

--- TipsAndTricksAFNetworking/TnTSignUpViewController.m
+++ TipsAndTricksAFNetworking/TnTSignUpViewController.m
@@ -50,130 +50,9 @@
 }
 - (IBAction)signup:(id)sender {
     
-    /*
-    [self.usernameField resignFirstResponder];
-    [self.emailField resignFirstResponder];
-    
-    NSString *username = [self.usernameField text];
-    NSString *email = [self.emailField text];
-  
-    NSURL *userRESTEndPoint = [TipsandTricks createURLForPath:@"/entity/user"];
     
     
-    NSMutableURLRequest *userCreationRequest = [[NSMutableURLRequest alloc]initWithURL:userRESTEndPoint];
     
-    [userCreationRequest setHTTPMethod:@"POST"];
-   
-    
-    NSDictionary *requestBodyDictionary = @{
-        @"_links":@{
-            @"type":@{
-                @"href":@"http://tntfoss-vivekvpandya.rhcloud.com/rest/type/user/user"
-    		}
-        },
-        @"name":@[@{@"value":username}],
-        @"mail":@[@{@"value":email}],
-        @"status":@[@{@"value":@"0"}] // 0 -> blocked state 1 -> active state
-        
-        
-        };
-    NSData *requestBodyData  = [NSJSONSerialization dataWithJSONObject:requestBodyDictionary options:kNilOptions error:NULL];
-    
-    [userCreationRequest setHTTPBody:requestBodyData];
-    [userCreationRequest setAllHTTPHeaderFields:@{@"Content-Type":@"application/hal+json",@"Authorization":@"Basic cm9vdDprfjNpVHJhaEQ="}];
-    
-   
-    
-    // here we need to pass root credential with user POST request
-    // It shoud not be like that
-    // for more details ferer this https://www.drupal.org/node/2291055
-    
-    
-    NSURLSession *session = [NSURLSession sharedSession];
-    
-    NSURLSessionDataTask *POSTUserTask = [session dataTaskWithRequest:userCreationRequest completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
-        
-        
-        if (!error){
-        NSDictionary *responseData = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:NULL];
-        NSString *responseString = [NSString stringWithFormat:@"%@",responseData];
-        
-        
-        NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)response;
-        
-        if (httpResponse.statusCode == 201) {
-            
-            dispatch_async(dispatch_get_main_queue(), ^{
-                [self.activityIndicator stopAnimating];
-                UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"Sign-up Successful" message:@"A mail with further details has been sent to your mail id" delegate:self cancelButtonTitle:nil otherButtonTitles:@"OK", nil];
-                [alert show];
-                
-                
-
-            });
-            
-            
-        }
-        else if(httpResponse.statusCode == 422) {
-            
-            dispatch_async(dispatch_get_main_queue(), ^{
-                
-                [self.activityIndicator stopAnimating];
-                
-                UIAlertView *alertForUnprocessable = [[UIAlertView alloc]initWithTitle:@"Can not create user" message:responseString delegate:self
-                                                                     cancelButtonTitle:nil otherButtonTitles:@"OK", nil];
-                alertForUnprocessable.tag = 1;
-                [alertForUnprocessable show];
-            });
-            
-            
-        }
-        else{
-        
-            dispatch_async(dispatch_get_main_queue(), ^{
-                
-                [self.activityIndicator stopAnimating];
-                
-                NSLog(@"%lu",(long)httpResponse.statusCode);
-                
-                UIAlertView *alertForFaliure = [[UIAlertView alloc]initWithTitle:@"Can not create user" message:responseString delegate:self
-                                                               cancelButtonTitle:nil otherButtonTitles:@"OK", nil];
-                alertForFaliure.tag = 2;
-                
-                [alertForFaliure show];
-            });
-            
-        }
-            
-        }
-        else{
-            dispatch_async(dispatch_get_main_queue(), ^{
-                
-                [self.activityIndicator stopAnimating];
-                
-                NSLog(@"error");
-                UIAlertView *alertForError = [[UIAlertView alloc]initWithTitle:@"Can not create user" message:error.localizedDescription delegate:self
-                                                             cancelButtonTitle:nil otherButtonTitles:@"OK", nil];
-                
-                alertForError.tag = 3;
-                [alertForError show];
-
-                
-            });
-
-            
-            
-        
-            
-        }
-        
-        
-    }];
-    
-    [POSTUserTask resume];
-    [self.activityIndicator startAnimating];
-    
-    */
     
     
     [self.usernameField resignFirstResponder];
@@ -203,7 +82,9 @@
     [manager.requestSerializer setValue:@"application/hal+json" forHTTPHeaderField:@"Content-Type" ];
     [manager.requestSerializer setValue:@"Basic cm9vdDprfjNpVHJhaEQ=" forHTTPHeaderField:@"Authorization"];
     
-    
+    // here we need to pass root credential with user POST request
+    // It shoud not be like that
+    // for more details ferer this https://www.drupal.org/node/2291055
     
 NSURLSessionDataTask *postUser = [manager POST:@"http://tntfoss-vivekvpandya.rhcloud.com/entity/user" parameters:requestBodyDictionary success:^(NSURLSessionDataTask *task, id responseObject) {
     
